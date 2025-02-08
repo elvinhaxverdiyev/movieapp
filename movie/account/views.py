5e07d3d9f949a232fcae321a542d041658c5572b
Elvin <elvinhaxverdiyev777@gamil.com>
2025-02-08 18:05:42 +0400

    fixed some bugs and added register method

--- movie/account/views.py
+++ movie/account/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 # Create your views here.
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("home")
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -21,8 +24,42 @@ def login_view(request):
     return render(request, "login.html")
 
 
+
+
 def register_view(request):
-    return render(request, "register.html")
+    if request.user.is_authenticated:
+        return redirect("home")
+    
+    if request.method == "POST":
+        
+        username = request.POST.get("username", "").strip()
+        email = request.POST.get("email", "").strip()
+        firstname = request.POST.get("firstname", "").strip()
+        lastname = request.POST.get("lastname", "").strip()
+        password = request.POST.get("password", "")
+        repassword = request.POST.get("repassword", "")
+
+        if password != repassword:
+            return render(request, "register.html", {"error": "Passwords do not match"})
+
+        if User.objects.filter(username=username).exists():
+            return render(request, "register.html", {"error": "Username already exists"})
+
+        if User.objects.filter(email=email).exists():
+            return render(request, "register.html", {"error": "This email already exists"})
+
+        user = User.objects.create_user(
+            username=username,
+            email=email, 
+            first_name=firstname,
+            last_name=lastname, 
+            password=password
+        )
+        user.save()
+
+        return redirect("login")  
+    return render(request, "register.html")  
+
 
 
 def logout_view(request):
